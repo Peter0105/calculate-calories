3626b1657603c69d1c3c436770fe1c6e8912409b
SeoChanghyun <boy994562@gmail.com>
2017-08-03 14:37:20 +0000

    Calculating calories

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,47 @@
+class HomeController < ApplicationController
+  def index
+    food = ['apple', 'ban', 'cat', 'dog', 'ear']
+    calories = [1000, 5000, 30000, 5, 100]
+    @index = (0..4).to_a.sample(3)
+    @breakfast = []
+    @break_cal = []
+    @sum_break_cal = 0
+    
+    @index2 = (0..4).to_a.sample(3)
+    @lunch = []
+    @lunch_cal = []
+    @sum_lunch_cal = 0
+    
+    @index3 = (0..4).to_a.sample(3)
+    @dinner = []
+    @dinner_cal = []
+    @sum_dinner_cal = 0
+    
+    @sum_calories = 0
+    
+    # breakfast
+  for i in 0..2
+    @breakfast.push(food[@index[i]]); 
+    @break_cal.push(calories[@index[i]]);
+    @sum_break_cal += calories[@index[i]]
+  end
+  
+  # lunch
+  for i in 0..2
+    @lunch.push(food[@index2[i]]); 
+    @lunch_cal.push(calories[@index2[i]]);
+    @sum_lunch_cal += calories[@index2[i]]
+  end
+  
+  # dinner
+  for i in 0..2
+    @dinner.push(food[@index3[i]]); 
+    @dinner_cal.push(calories[@index3[i]]);
+    @sum_dinner_cal += calories[@index3[i]]
+  end
+  
+  #total calories
+  @sum_calories = @sum_break_cal + @sum_lunch_cal + @sum_dinner_cal
+  
+  end
+end
