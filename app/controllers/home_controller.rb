c2b38d09f2225af0b22f41e43a16d5374f54963f
SeoChanghyun <boy994562@gmail.com>
2017-08-07 15:40:37 +0000

    added CRUB-CR

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,27 +21,42 @@ class HomeController < ApplicationController
     
     # breakfast
   for i in 0..2
-    @breakfast.push(food[@index[i]]); 
-    @break_cal.push(calories[@index[i]]);
+    @breakfast.push(food[@index[i]]) 
+    @break_cal.push(calories[@index[i]])
+    @food = Food.new
+    @food.food = food[@index[i]]
+    @food.calories = calories[@index[i]]
+    @food.save
     @sum_break_cal += calories[@index[i]]
   end
   
   # lunch
   for i in 0..2
-    @lunch.push(food[@index2[i]]); 
-    @lunch_cal.push(calories[@index2[i]]);
+    @lunch.push(food[@index2[i]])
+    @lunch_cal.push(calories[@index2[i]])
+    @food = Food.new
+    @food.food = food[@index2[i]]
+    @food.calories = calories[@index2[i]]
+    @food.save
     @sum_lunch_cal += calories[@index2[i]]
   end
   
   # dinner
   for i in 0..2
-    @dinner.push(food[@index3[i]]); 
-    @dinner_cal.push(calories[@index3[i]]);
+    @dinner.push(food[@index3[i]])
+    @dinner_cal.push(calories[@index3[i]])
+    @food = Food.new
+    @food.food = food[@index3[i]]
+    @food.calories = calories[@index3[i]]
+    @food.save
     @sum_dinner_cal += calories[@index3[i]]
   end
   
   #total calories
   @sum_calories = @sum_break_cal + @sum_lunch_cal + @sum_dinner_cal
-  
+  end
+  def memory
+    @foods = Food.all
+    
   end
 end
